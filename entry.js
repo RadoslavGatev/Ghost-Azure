6e8c08366b8d67233d477ee3220ef3ebcd851a22
Dimitar Mazhlekov <d.mazhlekov@gmail.com>
2017-10-29 17:10:43 +0200

    Updated app insights env key

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
 process.env.server__port = process.env.PORT; // Get the port from named pipe
-let appInsightsKey = process.env.APP_INSIGHTS_KEY;
+let appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
 
 if (appInsightsKey) {
     let appInsights = require("applicationinsights");
