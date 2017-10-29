d5cf7fd5444e0927cf5e8e4735160cbe56759556
Dimitar Mazhlekov <d.mazhlekov@gmail.com>
2017-10-29 15:24:38 +0200

    Added Application Insights support

--- entry.js
+++ entry.js
@@ -1,2 +1,9 @@
-process.env.server__port = process.env.PORT;
+process.env.server__port = process.env.PORT; // Get the port from named pipe
+let appInsightsKey = process.env.APP_INSIGHTS_KEY;
+
+if (appInsightsKey) {
+    let appInsights = require("applicationinsights");
+    appInsights.setup(appInsightsKey).start();
+}
+
 require('./index.js')
