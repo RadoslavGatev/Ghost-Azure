855d705c340fb02c7a6f978f78b6f92d4fbe00d7
Dimitar Mazhlekov <d.mazhlekov@gmail.com>
2018-01-08 22:55:58 +0200

    Delete *

--- core/server/data/meta/rss_url.js
+++
@@ -1,7 +0,0 @@
-var urlService = require('../../services/url');
-
-function getRssUrl(data, absolute) {
-    return urlService.utils.urlFor('rss', {secure: data.secure}, absolute);
-}
-
-module.exports = getRssUrl;
--- core/server/lib/common/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.i18n = require('./i18n');
-module.exports.events = require('./events');
-module.exports.errors = require('./errors');
-module.exports.logging = require('./logging');
