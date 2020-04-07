2a9dbcefb43fe759f1cc8786636365234bf1af91
Radoslav Gatev <RadoslavGatev@users.noreply.github.com>
2020-04-07 10:56:13 +0000

    Add v3.13.0

---
+++ core/server/services/adapter-manager/index.js
@@ -0,0 +1,28 @@
+const AdapterManager = require('@tryghost/adapter-manager');
+const getAdapterServiceConfig = require('./config');
+const config = require('../../config');
+
+const adapterManager = new AdapterManager({
+    loadAdapterFromPath: require,
+    pathsToAdapters: [
+        '', // A blank path will cause us to check node_modules for the adapter
+        config.getContentPath('adapters'),
+        config.get('paths').internalAdaptersPath
+    ]
+});
+
+adapterManager.registerAdapter('storage', require('ghost-storage-base'));
+adapterManager.registerAdapter('scheduling', require('../../adapters/scheduling/SchedulingBase'));
+adapterManager.registerAdapter('sso', require('../../adapters/sso/Base'));
+
+module.exports = {
+    getAdapter(adapterType) {
+        const adapterServiceConfig = getAdapterServiceConfig(config);
+
+        const adapterSettings = adapterServiceConfig[adapterType];
+        const activeAdapter = adapterSettings.active;
+        const activeAdapterConfig = adapterSettings[activeAdapter];
+
+        return adapterManager.getAdapter(adapterType, activeAdapter, activeAdapterConfig);
+    }
+};
--- core/server/services/auth/session/index.js
+++ core/server/services/auth/session/index.js
@@ -1,14 +1,49 @@
-module.exports = {
-    // @TODO: expose files/units and not functions of units
-    get createSession() {
-        return require('./middleware').createSession;
-    },
+const adapterManager = require('../../adapter-manager');
+const createSessionService = require('@tryghost/session-service');
+const sessionFromToken = require('@tryghost/mw-session-from-token');
+const createSessionMiddleware = require('./middleware');
 
-    get destroySession() {
-        return require('./middleware').destroySession;
-    },
+const expressSession = require('./express-session');
 
-    get authenticate() {
-        return require('./middleware').authenticate;
+const models = require('../../../models');
+const urlUtils = require('../../../lib/url-utils');
+const url = require('url');
+
+function getOriginOfRequest(req) {
+    const origin = req.get('origin');
+    const referrer = req.get('referrer') || urlUtils.getAdminUrl() || urlUtils.getSiteUrl();
+
+    if (!origin && !referrer) {
+        return null;
     }
-};
+
+    if (origin) {
+        return origin;
+    }
+
+    const {protocol, host} = url.parse(referrer);
+    if (protocol && host) {
+        return `${protocol}//${host}`;
+    }
+    return null;
+}
+
+const sessionService = createSessionService({
+    getOriginOfRequest,
+    getSession: expressSession.getSession,
+    findUserById({id}) {
+        return models.User.findOne({id});
+    }
+});
+
+module.exports = createSessionMiddleware({sessionService});
+
+const ssoAdapter = adapterManager.getAdapter('sso');
+// Looks funky but this is a "custom" piece of middleware
+module.exports.createSessionFromToken = sessionFromToken({
+    callNextWithError: false,
+    createSession: sessionService.createSessionForUser,
+    findUserByLookup: ssoAdapter.getUserForIdentity,
+    getLookupFromToken: ssoAdapter.getIdentityFromCredentials,
+    getTokenFromRequest: ssoAdapter.getRequestCredentials
+});
