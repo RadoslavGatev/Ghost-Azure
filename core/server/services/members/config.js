381f8010198ba4794522bfcb5b6bb72a3b2fadff
Radoslav Gatev <RadoslavGatev@users.noreply.github.com>
2020-06-16 11:14:03 +0000

    Add v3.20.0

--- core/server/services/members/config.js
+++ core/server/services/members/config.js
@@ -53,7 +53,8 @@ class MembersConfigProvider {
             AUD: '$',
             CAD: '$',
             GBP: '£',
-            EUR: '€'
+            EUR: '€',
+            INR: '₹'
         };
         const defaultPriceData = {
             monthly: 0,
@@ -131,10 +132,6 @@ class MembersConfigProvider {
             return null;
         }
 
-        if (!stripePaymentProcessor.config.public_token || !stripePaymentProcessor.config.secret_token) {
-            return null;
-        }
-
         // NOTE: "Complimentary" plan has to be first in the queue so it is created even if regular plans are not configured
         stripePaymentProcessor.config.plans.unshift(COMPLIMENTARY_PLAN);
 
@@ -156,6 +153,10 @@ class MembersConfigProvider {
             stripePaymentProcessor.config.public_token,
             stripePaymentProcessor.config.secret_token
         );
+
+        if (!stripeApiKeys.publicKey || !stripeApiKeys.secretKey) {
+            return null;
+        }
 
         return {
             publicKey: stripeApiKeys.publicKey,
