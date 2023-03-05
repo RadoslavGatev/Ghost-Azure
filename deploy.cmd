5a7250bdbf88453494a18107b55458c62e13b5ca
Radoslav Gatev <RadoslavGatev@users.noreply.github.com>
2023-03-05 03:07:09 +0200

    test

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,7 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! config set scripts-prepend-node-path true
-  ::  call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd !NPM_CMD! install --production
   call :ExecuteCmd !NPM_CMD! rebuild --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
